86d1a0d2bbe4caafdad2fd70c0684d50180bc012
Rob Piwko <rdp6158@rit.edu>
2019-02-25 16:32:27 -0500

    most of es5 implimentation

--- js/classes.js
+++ js/classes.js
@@ -1,37 +1,47 @@
-//circles function with move, relfect, and follow player functions
-class Circle{
-    constructor(color = 0xFF0000,alpha){
-		this.posX = 320;
-		this.posY = 200;
-		this.originX = this.posX;
-		this.originY = this.posY;
-    	this.radius = Math.floor(Math.random() * 5) + 1;
-		this.incX = (Math.random() * 10) - 5;
-		this.incY = (Math.random() * 10) - 5;
-		this.color = color;
-		this.actve = true;
-		this.alpha = alpha;
-    }
-	
-	moveCircle(radius) {
-		this.posX += this.incX;
-		this.posY += this.incY;
-		if(this.posX < 0 - this.radius || this.posX > 640){
-			this.posX = this.originX;
-			this.posY = this.originY;
+var app = app || {};
+
+app.classes = (function(){
+
+
+	//circles function with move, relfect, and follow player functions
+	class Circle{
+		constructor(color = 0xFF0000,alpha){
+			this.posX = 320;
+			this.posY = 200;
+			this.originX = this.posX;
+			this.originY = this.posY;
+			this.radius = Math.floor(Math.random() * 5) + 1;
+			this.incX = (Math.random() * 10) - 5;
+			this.incY = (Math.random() * 10) - 5;
+			this.color = color;
+			this.actve = true;
+			this.alpha = alpha;
 		}
-		if(this.posY < 0 - this.radius || this.posY > 400){
-			this.posX = this.originX;
-			this.posY = this.originY;
+
+		moveCircle(radius) {
+			this.posX += this.incX;
+			this.posY += this.incY;
+			if(this.posX < 0 - this.radius || this.posX > 640){
+				this.posX = this.originX;
+				this.posY = this.originY;
+			}
+			if(this.posY < 0 - this.radius || this.posY > 400){
+				this.posX = this.originX;
+				this.posY = this.originY;
+			}
+			drawCtx.save();
+			drawCtx.globalAlpha = this.alpha;
+			drawCtx.beginPath();
+			drawCtx.fillStyle = this.color;
+			drawCtx.arc(this.posX, this.posY, radius, 0, Math.PI*2, true); 
+			drawCtx.closePath();
+			drawCtx.fill();
+			drawCtx.restore();
 		}
-		drawCtx.save();
-		drawCtx.globalAlpha = this.alpha;
-		drawCtx.beginPath();
-		drawCtx.fillStyle = this.color;
-		drawCtx.arc(this.posX, this.posY, radius, 0, Math.PI*2, true); 
-		drawCtx.closePath();
-		drawCtx.fill();
-		drawCtx.restore();
+
+	}
+	return{
+		
 	}
 
-}
+})();
